3df9a41e19144ecf9f57e397beb458e6015d4ef7
Indra Adi Baskara <indraadi3@gmail.com>
2023-07-12 15:16:52 +0700

    🏗️  setup basic design and test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./vitest.config.ts"],
+  },
+});
